84f9e60381a6f4e63563411cd9c0b2ae29b9d7c4
Rupinder-17 <rupinderrandhey90@gmail.com>
2025-05-29 22:28:39 +0530

    Add NavBar and Skills components to the project

---
+++ src/app/components/NavBar.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export const NavBar = () => {
+  return (
+    <>
+    <div>NavBar</div>
+    <div className='flex'>
+        <ul>Home</ul>
+        <ul>Projects</ul>
+        <ul>Skills</ul>
+        <ul>About</ul>
+        <ul>Contect</ul>
+    </div>
+    </>
+  )
+}
