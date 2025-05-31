a18239c4bc8b871dfc7aaae277fc31dde17a40aa
Rupinder-17 <rupinderrandhey90@gmail.com>
2025-05-31 08:35:22 +0530

    feat: update dependencies and enhance components with new features and animations

--- src/app/components/Container.tsx
+++ src/app/components/Container.tsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export const Container = ({ className = " bg-amber-50" ,children}) => {
   return (
-    <div className={`h-screen w-full ${className}  py-8`}>{children}</div>
+    <div className={`h-screen w-full ${className}  overflow-hidden py-8`}>{children}</div>
   )
 }
