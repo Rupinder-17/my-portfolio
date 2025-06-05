1fe2e868f6d377794715d29a8a9346bb3b327f2e
Rupinder-17 <rupinderrandhey90@gmail.com>
2025-06-05 15:14:40 +0530

    feat: replace Contect component content with BeamsBackground for enhanced visual appeal

--- src/app/components/Contect.tsx
+++ src/app/components/Contect.tsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import { Container } from './Container'
+import { BeamsBackground } from "@/components/ui/beams-background"
 
-export const Contect = () => {
-  return (
-    <div id='contact'>
-      <Container className=''>
-        <h1>Contecyt me</h1>
-      </Container>
-    </div>
-  )
+
+export const Contect =() =>{
+  return < BeamsBackground />
 }
+
+// export const Contect = () => {
+//   return (
+//     <div id='contact'>
+//       <Container className=''>
+//         <h1>Contecyt me</h1>
+//       </Container>
+//     </div>
+//   )
+// }
